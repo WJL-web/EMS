f93b33a3d77b28b6f78ae27d5900d6d346ff93df
bugpunk2077 <167391135+bugpunk2077@users.noreply.github.com>
2025-04-17 16:37:27 +0800

    Update Student.java
    用Lombok对setId 和setName 简化

--- edumanagement/src/main/java/com/example/edumanagement/entity/Student.java
+++ edumanagement/src/main/java/com/example/edumanagement/entity/Student.java
@@ -1,15 +1,19 @@
 package com.example.edumanagement.entity;
 
+import lombok.Setter;
+
 public class Student {
+    @Setter
     private Integer id;
+    @Setter
     private String name;
     private String student_number;
 
     // Getter 和 Setter
     public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
+
     public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+
     public String getStudent_number() { return student_number; }
     public void setStudent_number(String studentNumber) { this.student_number = student_number; }
 }
