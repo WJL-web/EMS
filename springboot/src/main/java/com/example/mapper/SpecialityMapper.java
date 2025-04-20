ea77ea3de206409b4d2885cea613494fad7bc9f4
bugpunk2077 <167391135+bugpunk2077@users.noreply.github.com>
2025-04-20 18:15:41 +0800

    专业信息模块

---
+++ springboot/src/main/java/com/example/mapper/SpecialityMapper.java
@@ -0,0 +1,38 @@
+package com.example.edumanagement.mapper;
+
+import com.example.edumanagement.entity.Speciality;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * 操作admin相关数据接口
+*/
+public interface SpecialityMapper {
+
+    /**
+      * 新增
+    */
+    int insert(Speciality speciality);
+
+    /**
+      * 删除
+    */
+    int deleteById(Integer id);
+
+    /**
+      * 修改
+    */
+    int updateById(Speciality speciality);
+
+    /**
+      * 根据ID查询
+    */
+    Speciality selectById(Integer id);
+
+    /**
+      * 查询所有
+    */
+    List<Speciality> selectAll(Speciality speciality);
+
+}
