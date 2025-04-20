8395867dd328dbda6fb3c1ee850f34483b129ecf
bugpunk2077 <167391135+bugpunk2077@users.noreply.github.com>
2025-04-20 18:17:23 +0800

    专业信息模块

---
+++ springboot/src/main/java/com/example/service/SpecialityService.java
@@ -0,0 +1,84 @@
+package com.example.edumanagement.service;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.example.edumanagement.common.Constants;
+import com.example.edumanagement.common.enums.ResultCodeEnum;
+import com.example.edumanagement.common.enums.RoleEnum;
+import com.example.edumanagement.entity.Account;
+import com.example.edumanagement.entity.Speciality;
+import com.example.edumanagement.exception.CustomException;
+import com.example.edumanagement.mapper.SpecialityMapper;
+import com.example.edumanagement.utils.TokenUtils;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 管理员业务处理
+ **/
+@Service
+public class SpecialityService {
+
+    @Resource
+    private SpecialityMapper specialityMapper;
+
+    /**
+     * 新增
+     */
+    public void add(Speciality speciality) {
+        specialityMapper.insert(speciality);
+    }
+
+    /**
+     * 删除
+     */
+    public void deleteById(Integer id) {
+        specialityMapper.deleteById(id);
+    }
+
+    /**
+     * 批量删除
+     */
+    public void deleteBatch(List<Integer> ids) {
+        for (Integer id : ids) {
+            specialityMapper.deleteById(id);
+        }
+    }
+
+    /**
+     * 修改
+     */
+    public void updateById(Speciality speciality) {
+        specialityMapper.updateById(speciality);
+    }
+
+    /**
+     * 根据ID查询
+     */
+    public Speciality selectById(Integer id) {
+        return specialityMapper.selectById(id);
+    }
+
+    /**
+     * 查询所有
+     */
+    public List<Speciality> selectAll(Speciality speciality) {
+        return specialityMapper.selectAll(speciality);
+    }
+
+    /**
+     * 分页查询
+     */
+    public PageInfo<Speciality> selectPage(Speciality speciality, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<Speciality> list = specialityMapper.selectAll(speciality);
+        return PageInfo.of(list);
+    }
+
+
+
+}
