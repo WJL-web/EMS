0c7908f2a9e4ad0f04a4dccd9acea6fe9efce861
WJL-web <3462829838@qq.com>
2025-04-21 00:04:38 +0800

    Update Home.vue

--- vue/src/views/manager/Home.vue
+++ vue/src/views/manager/Home.vue
@@ -5,8 +5,9 @@
     </div>
 
     <div style="display: flex; margin: 10px 0">
+
       <div style="width: 50%;" class="card">
-        <div style="margin-bottom: 30px; font-size: 20px; font-weight: bold">公告列表</div>
+        <div style="margin-bottom: 30px; font-size: 20px; font-weight: bold">教务通知</div>
         <div >
           <el-timeline  reverse slot="reference">
             <el-timeline-item v-for="item in notices" :key="item.id" :timestamp="item.time">
@@ -21,6 +22,25 @@
           </el-timeline>
         </div>
       </div>
+
+      <div style="width: 50%;" class="card">
+        <div style="margin-bottom: 30px; font-size: 20px; font-weight: bold">考试安排</div>
+        <div >
+          <el-timeline  reverse slot="reference">
+            <el-timeline-item v-for="item in examplans" :key="item.id" :timestamp="item.time">
+              <el-popover
+                  placement="right"
+                  width="200"
+                  trigger="hover"
+                  :content="item.content">
+                <span slot="reference">{{ item.name }}</span>
+              </el-popover>
+            </el-timeline-item>
+          </el-timeline>
+        </div>
+      </div>
+
+
     </div>
   </div>
 </template>
@@ -32,12 +52,20 @@ export default {
   data() {
     return {
       user: JSON.parse(localStorage.getItem('xm-user') || '{}'),
-      notices: []
+      notices: [],
+      examplans: []
     }
   },
   created() {
     this.$request.get('/notice/selectAll').then(res => {
       this.notices = res.data || []
+    })
+    this.$request.get('/examplan/selectAll').then(res => {
+      if (res.code === '200') {
+        this.examplans = res.data || []
+      } else {
+        this.$message.error(res.msg)
+      }
     })
   }
 }
